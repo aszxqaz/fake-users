178b4eb1e67c5644d05b68a7bc8e82e82b49a0ee
Maxim Butenko <maxim.butenko.drive@gmail.com>
2024-04-24 04:35:13 +0400

    first try

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
+
 @Module({
-    imports: [UsersModule],
+    imports: [
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '..', 'client'),
+            exclude: ['/api*'],
+        }),
+        UsersModule,
+    ],
     controllers: [AppController],
     providers: [],
 })
